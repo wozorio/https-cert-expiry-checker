164dda34f106bfd9c6c582a060fb0f4e9eacd887
Wellington Ozorio <Wellington.Ozorio@br.bosch.com>
2023-05-19 14:27:02 -0300

    Update

--- https_cert_expiry_checker.py
+++ https_cert_expiry_checker.py
@@ -113,11 +113,10 @@ def send_mail(url: str, email: dict, cert_expiry_date: datetime.date, days_befor
         from_email=email["sender"],
         to_emails=email["recipients"],
         subject=f"TLS certificate for {url} about to expire",
-        html_content=f"""<p> Dear Site Reliability Engineer, </p> \
+        html_content=f"<p> Dear Site Reliability Engineer, </p> \
             <p> This is to notify you that the TLS certificate for <b>{url}</b> will expire on {cert_expiry_date}. </p> \
             <p> Please ensure a new certificate is ordered and installed in a timely fashion. There are {days_before_cert_expires} days remaining. </p> \
-            <p> Sincerely yours, <br>DevOps Team </p>
-            """,
+            <p> Sincerely yours, <br>DevOps Team </p>",
     )
     try:
         sendgrid = SendGridAPIClient(os.getenv("SENDGRID_API_KEY"))
